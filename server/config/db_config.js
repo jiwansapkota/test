de2fbe2540b9ca15c96b8501995d0be5d038ad81
jiwansapkota <36408400+jiwansapkota@users.noreply.github.com>
2021-05-21 15:40:09 +0545

    Delete server/config directory

--- server/config/db_config.js
+++
@@ -1,9 +0,0 @@
-module.exports={
-    config:{
-        "host": "202.51.74.42",
-        "port": 6303,
-        "user": "fullstack",
-        "password": "wZ&&h5vu",
-        "database": "smtmtestdb",
-      }
-}
